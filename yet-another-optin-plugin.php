f4a303b4cdac070646f2cf1950b807cd03bf676b
ido barnea <barbareshet@gmail.com>
2016-12-20 15:02:26 +0200

    init

---
+++ yet-another-optin-plugin.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: Yet Another Optin Plugin For Wordpress
+ * Plugin URI:  http://idowebservices/yet-another-optin-plugin
+ * Description: Basic WordPress Plugin Header Comment
+ * Version:     1.0.0
+ * Author:      Ido Barnea
+ * Author URI:  http://barbareshet.co.il
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: yaopfwp
+ */
+//Exit if accessed directly
+if (!defined('ABSPATH')){
+    exit;
+}
+if( !defined( 'YAOPFWP_VER' ) )
+    define( 'YAOPFWP_VER', '1.0.0' );
